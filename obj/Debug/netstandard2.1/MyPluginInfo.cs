46b569e75ffd058822111ef682417538998b64da
Flavien <flavienbriendo@gmail.com>
2024-06-01 17:35:42 +0200

    fix: clean code

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace MyFirstMod
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "Flav20.AliasPlugin";
-        public const string PLUGIN_NAME = "alias plugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
